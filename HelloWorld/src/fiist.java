6ff6ba8fe6f6c4bdb587a94f390557aa1f80131a
Jiwon Kim <bib060819@naver.com>
2020-11-17 16:21:39 +0900

    sjkang branch change a code

--- HelloWorld/src/fiist.java
+++ HelloWorld/src/fiist.java
@@ -4,6 +4,9 @@ public class fiist {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("kang seong jin");
+		System.out.println("computer engineering");
+		
+		
 		
 
 	}
